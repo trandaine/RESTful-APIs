2f7c573ce40d7037a85248a8cad058cc04b8f825
Tran Dai <tranquangdai03072004@outlook.com.vn>
2024-10-24 16:53:01 +0700

    Update connectionstring

--- sever/sever.js
+++ sever/sever.js
@@ -2,25 +2,12 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     Task = require('./api/models/todoListModel');
+    const mongoose = require("mongoose");
+    const bodyParser = require('body-parser');
 
-const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
-
-const username = "sa";
-const password = "Dai2018";
-const connectionStr = `mongodb://${username}:${password}@localhost:27017/Tododb?authSource=admin`; 
-mongoose.set("strictQuery", true); 
-const options = { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-}; 
-
-mongoose.connect(connectionStr, options, (error, connection) => { 
-    if (error) { 
-        console.error("Error connecting to MongoDB:", error); 
-    } else { 
-        console.log("Connected to MongoDB!"); 
-    } 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://sa:Dai2018@localhost:27017/', {
+    dbName: 'Tododb',
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
