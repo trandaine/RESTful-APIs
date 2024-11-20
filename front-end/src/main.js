b787f07860144a556a6ccb93b785e9c419f6063c
Trần Quang Đại <tranquangdai03072004@outlook.com.vn>
2024-11-20 11:21:55 +0700

    Update sourcecode

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+import 'semantic-ui-css/semantic.css'
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: function (h) { return h(App) },
+  router,
+  render: h => h(App)
 }).$mount('#app')
+
--- sever/sever.js
+++ sever/sever.js
@@ -27,6 +27,13 @@ mongoose.connect(connectionStr, options, (error, connection) => {
     } 
 });
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
