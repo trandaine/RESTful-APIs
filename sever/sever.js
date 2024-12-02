2d2b0ff6109b86bd53e0950fa5cfe49bd26664e8
William Wu <19112004vutranquangminh@gmail.com>
2024-12-02 22:44:55 +0700

    done translate

--- sever/sever.js
+++ sever/sever.js
@@ -18,15 +18,15 @@ app.use(cors({
 }));
 
 // CORS headers (Consider refining these for production)
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Middleware
 app.use(bodyParser.json());
