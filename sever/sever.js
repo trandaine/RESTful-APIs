1101749d211ad11f3b44a0db0e529b55e0e9484a
Trần Quang Đại <tranquangdai03072004@outlook.com.vn>
2024-12-02 20:48:42 +0700

    Update CORS policy

--- sever/sever.js
+++ sever/sever.js
@@ -17,6 +17,17 @@ app.use(cors({
   allowedHeaders: 'Content-Type,Authorization', // Allow the Authorization header
 }));
 
+// CORS headers (Consider refining these for production)
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
